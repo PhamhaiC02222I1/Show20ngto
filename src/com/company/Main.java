6ce23cd31e9f8b813687d4ca8cccfdf731407818
hai <humg.ola@gmail.com>
2022-03-24 20:40:01 +0700

    up

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Số lượng số nguyên tố cần in ra:");
+            int numbers = input.nextInt();
+
+            int count = 0;
+            int n = 2;
+
+            while (count <= numbers) {
+                boolean flag = false;
+                for (int i = 2; i < n; i++) {
+                    if (n % i == 0) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    System.out.println(n);
+                    count++;
+                } else {
+                    flag = false;
+                }
+                n++;
+            }
+        }
+    }
+
+
